ddeff15c48616af0102d00405dfc3ec70830c1fc
Reza Qavi <rezaqavi1378@gmail.com>
2021-07-19 09:39:20 +0430

    add route for record page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Cough from './components/cough/Cough';
+
 import $ from 'jquery';
 import './components/static/Fonts/vazir-fonts/fonts.css';
+import Record from './components/record/Record';
 
 
 class App extends React.Component {
@@ -16,7 +18,8 @@ class App extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact="/" component={Cough}></Route>
+          <Route exact={true} path="/" component={Cough}></Route>
+          <Route exact={true} path="/record" component={Record}></Route>
         </Switch>
       </Router>
     );
