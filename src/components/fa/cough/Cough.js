08ac96ff187f5207bd319975e3d89c0eabf50988
Reza Qavi <rezaqavi1378@gmail.com>
2021-07-19 17:39:24 +0430

    Fix design fa page

--- src/components/fa/cough/Cough.js
+++ src/components/fa/cough/Cough.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import $ from 'jquery';
-
 import Header from '../common/header/Header';
 import Footer from '../common/footer/Footer';
 import Title from './title/Title';
