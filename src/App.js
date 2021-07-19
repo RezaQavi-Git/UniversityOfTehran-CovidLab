168267224e7b45c2c89cc2f48d83b24757fbdbe6
Reza Qavi <rezaqavi1378@gmail.com>
2021-07-19 13:11:54 +0430

    Add en routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Cough from './components/cough/Cough';
 
 import $ from 'jquery';
 import './components/static/Fonts/vazir-fonts/fonts.css';
-import Record from './components/record/Record';
 
+
+import Cough from './components/fa/cough/Cough';
+import Record from './components/fa/record/Record';
+import CoughEn from './components/en/cough/Cough';
+import RecordEn from './components/en/record/Record';
 
 class App extends React.Component {
 
@@ -20,6 +23,8 @@ class App extends React.Component {
         <Switch>
           <Route exact={true} path="/" component={Cough}></Route>
           <Route exact={true} path="/record" component={Record}></Route>
+          <Route exact={true} path="/en" component={CoughEn}></Route>
+          <Route exact={true} path="/record/en" component={RecordEn}></Route>
         </Switch>
       </Router>
     );
