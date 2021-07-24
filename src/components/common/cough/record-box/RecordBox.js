4cc41d2e3b28f76bcaa38b2ed20e666478e34ef1
Reza Qavi <rezaqavi1378@gmail.com>
2021-07-24 15:41:33 +0430

    add change in en/fa form of design

--- src/components/common/cough/record-box/RecordBox.js
+++ src/components/common/cough/record-box/RecordBox.js
@@ -24,11 +24,11 @@ class RecordBox extends React.Component {
                                 این ویروس با دست آلوده یا عطسه، سرفه و حتی قطره های تنفسی از طریق دهان، بینی و چشم به افراد منتقل می‌شود. تنگی نفس، خستگی و بدن درد، اختلال در بویایی و چشایی و مشکلات گوارشی از جمله علایم  بیماری کووید ۱۹ است  و بیش از ۸۰ درصد مبتلایان به ویروس نیز دچار بیماری خفیف می‌شوند.
                             </p>
                         </div>
-                        
                         <div className="description-box-record">
                             <Link to="/record"> <img src="https://img.icons8.com/ios/50/4a90e2/play-record--v2.png" className="microphone" alt="mic"/>  </Link> 
                             <p>ضبط صدا</p>
                         </div>
+                        
                     </div>
                 </React.Fragment>
             )
@@ -36,6 +36,10 @@ class RecordBox extends React.Component {
             return(
                 <React.Fragment>
                     <div className="description-box">
+                        <div className="description-box-record">
+                            <Link to="/record/en"> <img src="https://img.icons8.com/ios/50/4a90e2/play-record--v2.png" className="microphone" alt="mic"/>  </Link> 
+                            <p>Record Voice</p>
+                        </div>
                         <div className="description-box-description">
                             <p className="en">
                             Yazd, with a population of 1.2 million, has 10 cities. According to the university spokesman, the trend of spreading the corona virus, which causes Covid 19 disease, is increasing in the province.
@@ -43,10 +47,6 @@ class RecordBox extends React.Component {
                             According to IRNA, the coronavirus, which causes Covid 19 disease, has been observed in Wuhan, China since late 2019 and was spread around the world in a short time; So that the World Health Organization in March (February 2020) confirmed the outbreak of a pandemic (global epidemic) of this disease Corona
                             The virus is transmitted to infected people through infected hands or sneezing, coughing and even respiratory drops through the mouth, nose and eyes. Shortness of breath, fatigue and body aches, olfactory and taste disturbances, and gastrointestinal problems are some of the symptoms of Covid 19 disease, and more than 80% of those infected with the virus also develop a mild illness.
                             </p>
-                        </div>
-                        <div className="description-box-record">
-                            <Link to="/record/en"> <img src="https://img.icons8.com/ios/50/4a90e2/play-record--v2.png" className="microphone" alt="mic"/>  </Link> 
-                            <p>Record Voice</p>
                         </div>
                         
                     </div>
