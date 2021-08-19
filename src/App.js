ba320efb6d9857d5b5b365ccb328b15f1a783984
Reza Qavi <rezaqavi1378@gmail.com>
2021-08-19 10:14:46 +0430

    Add Page link and components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import "./components/static/Fonts/vazir-fonts/fonts.css";
 import Home from "./components/home/Home";
 import Record from "./components/record/Record";
 import Team from "./components/team/Team";
+import About from "./components/about/About";
+import News from "./components/news/News";
+import Projects from "./components/projects/Projects";
+
 
 import "./App.css";
 
@@ -43,6 +47,36 @@ class App extends React.Component {
             exact={true}
             path="/team/en"
             render={(props) => <Team lang="en" {...props} />}
+          ></Route>
+          <Route
+            exact={true}
+            path="/about/fa"
+            render={(props) => <About lang="fa" {...props} />}
+          ></Route>
+          <Route
+            exact={true}
+            path="/about/en"
+            render={(props) => <About lang="en" {...props} />}
+          ></Route>
+          <Route
+            exact={true}
+            path="/projects/fa"
+            render={(props) => <Projects lang="fa" {...props} />}
+          ></Route>
+          <Route
+            exact={true}
+            path="/projects/en"
+            render={(props) => <Projects lang="en" {...props} />}
+          ></Route>
+          <Route
+            exact={true}
+            path="/news/fa"
+            render={(props) => <News lang="fa" {...props} />}
+          ></Route>
+          <Route
+            exact={true}
+            path="/news/en"
+            render={(props) => <News lang="en" {...props} />}
           ></Route>
         </Switch>
       </Router>
